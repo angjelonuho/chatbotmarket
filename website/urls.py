e43fe5ce31e5ed43a121cc9a58ecd53a7edad943
Angjelo Dell <angelonuuo@gmail.com>
2019-05-29 10:24:42 +0200

    initial commit

---
+++ website/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path 
+from . import views
+
+urlpatterns = [
+    path('' , views.index , name = 'index'),
+    path('cart' , views.cart , name = 'cart'),
+    path('login' , views.login , name = 'login'),
+    path('account' , views.account , name = 'account')
+   
+]
