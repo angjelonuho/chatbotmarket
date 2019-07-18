586fa52136db7adaccf6aa59ce212651b0a67c62
Angjelo <33224333+angjelonuho@users.noreply.github.com>
2019-07-18 17:35:47 +0200

    user registration complete

--- website/forms.py
+++ website/forms.py
@@ -5,21 +5,14 @@ from django.utils.translation import gettext_lazy as _
 
 class SignUpForm(UserCreationForm):
 
-    email = forms.EmailField(widget= forms.EmailInput(attrs={'class':'form-control','name': 'Your name'}))
-    password1 = forms.CharField(label="Card Number" ,widget= forms.PasswordInput(attrs={'class':'form-control'}))
-    password2 = forms.CharField(widget= forms.PasswordInput(attrs={'class':'form-control'}))
-  
+    email = forms.EmailField(label = "Email" , widget= forms.EmailInput(attrs={'class':'form-control'}))
+    password1 = forms.CharField(label = "Password" ,widget= forms.PasswordInput(attrs={'class':'form-control'}))
+    password2 = forms.CharField(label = "Confirm Password" ,widget= forms.PasswordInput(attrs={'class':'form-control'} , ))
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2' )
 
-        # labels = {
-        #  'username': _('Username'),
-        #  'email': _('E-mail Address'),
-        #  'password1' : _('Password'), 
-        #  'password2' : _('Confirm Password'),
-        # }
 
         widgets = {
           'username': forms.TextInput(attrs={'class':'form-control'}),
--- website/views.py
+++ website/views.py
@@ -1,7 +1,8 @@
-from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
 from website.forms import SignUpForm
-
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -9,7 +10,7 @@ def index(request):
 def cart(request):
 	return  render (request , 'chatbotmarket/cart.html')
 
-def auth_login(request):
+def auth_register(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
@@ -18,13 +19,18 @@ def auth_login(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('login')
+            messages.success(request, f'Welcome {username}!')
+            return redirect('index')
+            
     else:
+
         form = SignUpForm()
     return render(request, 'chatbotmarket/login.html', {'form': form})
 
 def account(request):
+
 	return render (request , 'chatbotmarket/account.html')
+
 def support(request):
     return render (request , 'chatbotmarket/support.html')
 def fanmarket(request):
